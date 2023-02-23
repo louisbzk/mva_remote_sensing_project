9b917a11c3bbf5a0d2ea189cdd31e710e2977ed2
Louis <louisbf6@gmail.com>
2023-02-23 13:51:50 +0100

    [+] Add speckle to clean images to generate training samples

--- utils/injectspeckle_amplitude.py
+++ utils/injectspeckle_amplitude.py
@@ -6,6 +6,8 @@ Created on Wed Apr  4 13:12:23 2018
 @author: emasasso
 """
 import numpy as np
+import os
+import matplotlib.pyplot as plt
 
 
 def injectspeckle_amplitude(img, L):
@@ -20,6 +22,14 @@ def injectspeckle_amplitude(img, L):
     return ima_speckle_amplitude
 
 
-im = np.load('denoised_lely.npy')
-speckled_image = injectspeckle_amplitude(im, 1)
-np.save('noisy_lely.npy', speckled_image)
+if __name__ == '__main__':
+    clean_imgs_dir = '../data/train/gt/npy'
+    noised_imgs_dir = '../data/train/raw/npy'
+    # for root, dirs, files in os.walk(clean_imgs_dir):
+    #     for filename in files:
+    #         im = np.load(os.path.join(root, filename)).astype('double')
+    #         speckled_im = injectspeckle_amplitude(im, 1)
+    #         np.save(os.path.join(os.path.join(noised_imgs_dir, filename)), speckled_im)
+    im = np.load(os.path.join(noised_imgs_dir, 'lely.npy'))
+    plt.imshow(np.log(im + 1/255), cmap='gray')
+    plt.show()
